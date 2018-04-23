8df7f469d6f051b9bcd91072036cdc58e8625a70
Rakesh Chatrath <rakesh@rakeshchatrath.me>
2018-04-22 22:46:10 -0500

    Initial up function

--- database/migrations/1.0.js
+++ database/migrations/1.0.js
@@ -3,4 +3,11 @@ const Promise = require("bluebird");
 const sqlite3 = require("sqlite3"); 
 const path = require('path'); 
 
-module.exports = {}; 
+module.exports = {
+  up: function() {
+    return new Promise(function(resolve, reject){
+      let db = new sqlite3.Database('./database/InvoiceApp.db');
+      db.run(`PRAGMA foreign_keys = ON`); 
+    });
+  };
+}; 
