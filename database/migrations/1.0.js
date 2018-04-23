375a4c0a9ad0f417fc5619d8a0f306beab7c101e
Rakesh Chatrath <rakesh@rakeshchatrath.me>
2018-04-22 22:50:11 -0500

    Finalize up function

--- database/migrations/1.0.js
+++ database/migrations/1.0.js
@@ -5,9 +5,36 @@ const path = require('path');
 
 module.exports = {
   up: function() {
-    return new Promise(function(resolve, reject){
+    return new Promise(function(resolve, reject) {
       let db = new sqlite3.Database('./database/InvoiceApp.db');
-      db.run(`PRAGMA foreign_keys = ON`); 
+      db.run(`PRAGMA foreign_keys = ON`);
+
+      db.serialize(function() {
+        db.run(`CREATE TABLE users (
+          id INTEGER PRIMARY KEY,
+          name TEXT,
+          email TEXT,
+          company_name TEXT,
+          password TEXT
+        )`);
+
+        db.run(`CREATE TABLE invoices (
+          id INTEGER PRIMARY KEY,
+          name TEXT,
+          user_id INTEGER,
+          paid NUMERIC,
+          FOREIGN KEY(user_id) REFERENCES users(id)
+        )`);
+
+        db.run(`CREATE TABLE transactions (
+          id INTEGER PRIMARY KEY,
+          name TEXT,
+          price INTEGER,
+          invoice_id INTEGER,
+          FOREIGN KEY(invoice_id) REFERENCES invoices(id)
+        )`);
+      });
+      db.close();
     });
-  };
+  },
 }; 
